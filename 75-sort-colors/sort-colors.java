e603d3432fb01203ec1f701378597549927cb8c3
Prince <128713671+ft-prince@users.noreply.github.com>
2023-07-24 17:36:09 +0530

    Time: 1 ms (20.50%) | Memory: 41.1 MB (57.34%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,15 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
 
-        //bubble sorting algorithm
+        //selection sorting algorithm
+       
          for(int counter=0;counter<nums.length-1;counter++){
-          for(int i=0;i<nums.length-1-counter;i++){
-              if(nums[i]>nums[i+1]){
-                  int temp=nums[i];
-                  nums[i]=nums[i+1];
-                  nums[i+1]=temp;
+           int min=counter;    
+         for(int i=counter+1;i<nums.length;i++){
+             if(nums[i]<nums[min]){
+              min=i;   
               }
-          }
+          }  
+          int temp=nums[min];
+            nums[min]=nums[counter];
+            nums[counter]=temp;
       }   
     // return nums; 
     }
