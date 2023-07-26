5166c53ab6181795073c0a616e0ebf2c4383dd60
Prince <128713671+ft-prince@users.noreply.github.com>
2023-07-27 00:43:59 +0530

    Time: 2 ms (91.67%) | Memory: 40.3 MB (96.48%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        return isBalanced(s);
+    }
+    public static boolean isBalanced(String s) {
+		
+		 Stack<Character> st=new Stack<>();
+         for (int i = 0; i < s.length(); i++) {
+             char x=s.charAt(i);
+        	 if(x=='(' || x=='{' || x=='[') 
+            	 st.push(x);
+            else if(!st.isEmpty()){
+        	 if((x==')' && st.peek()=='(') ||
+              (x=='}' && st.peek()=='{' )
+              ||( x==']'&& st.peek()=='[')){
+             st.pop();
+             }
+	           else  
+        		 return false;  
+             }
+            else  return false;             
+             }
+		if(st.empty()) {
+			return true;
+		}
+		 return false;
+	}
+
+}
