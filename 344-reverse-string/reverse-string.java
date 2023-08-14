da9a007530fd81f299920686dfd1dd6108967c7f
Prince <128713671+ft-prince@users.noreply.github.com>
2023-08-15 02:48:30 +0530

    Time: 5 ms (5.18%) | Memory: 49 MB (98.25%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<s.length;i++){
+            stack.push(s[i]);
+        }
+        int i=0;
+        while(!stack.isEmpty()){
+            s[i]=stack.pop();
+            i++;
+        }
+    }
+}
