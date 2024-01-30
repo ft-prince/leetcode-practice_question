f9dfd9d7584bd5cd24dda61c80c4d41fdd831172
Prince <128713671+ft-prince@users.noreply.github.com>
2024-01-31 02:05:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (55.09%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,35 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // for (int i = 0; i < nums.length; i++) {
-        //     if (nums[i]==target){
-        //         return i;
-        //     }
-        // }
-        int n=nums.length;
-        int left=0;
-        int right=n-1;
-        while(left<=right){
-            int mid=(right+left)/2;
-            if(nums[mid]==target)
+        // Initialize pointers for binary search
+        int left = 0;
+        int right = nums.length - 1;
+
+        // Binary search loop
+        while (left <= right) {
+            // Calculate mid index
+            int mid = left + (right - left) / 2;
+
+            // Check if target is found at mid
+            if (nums[mid] == target)
                 return mid;
-            
-          // left sorted
-            if(nums[mid]>=nums[left]){
-          if(nums[left]<=target && target<=nums[mid]){
-              right=mid-1;
-          }
-          else{
-              left=mid+1;
-          }
-            }
-            //right sorted array
-            else{
-               if(nums[mid]<=target && target<=nums[right]){
-            left=mid+1;
-          }
-         else   right=mid-1;              
+
+            // Check if left to mid is sorted
+            if (nums[left] <= nums[mid]) {
+                // If target is in the left sorted range, adjust right pointer
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                // If target is in the right unsorted range, adjust left pointer
+                else
+                    left = mid + 1;
+            } else {
+                // If mid to right is sorted
+                // If target is in the right sorted range, adjust left pointer
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                // If target is in the left unsorted range, adjust right pointer
+                else
+                    right = mid - 1;
             }
         }
-    return -1;     
+
+        // Target not found
+        return -1;
     }
 }
