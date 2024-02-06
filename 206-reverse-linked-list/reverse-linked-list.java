6385b564e1baf01a1958d78cc8dcd1cc1b2483c0
Prince <128713671+ft-prince@users.noreply.github.com>
2024-02-06 18:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (60.21%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -7,27 +7,34 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
-
-
-
  */
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode curr=head;
-        ListNode prev=null;
-while(curr!=null){
- /*
- 1->2->3->4
-  <-1
-  prev=2
-
- */
-    ListNode ahead=curr.next;
-    curr.next=prev;
-    prev=curr;
-    curr=ahead;
-    // ad=curr.next;
-}
-    return prev;
+        // Check if the list is empty or has only one node
+        if (head == null || head.next == null)
+            return head;
+        
+        // Initialize pointers to traverse the list
+        ListNode curr = head;
+        ListNode prev = null;
+        
+        // Iterate through the list, reversing pointers along the way
+        while (curr != null) {
+            // Store the next node temporarily
+            ListNode tmp = curr.next;
+            // Reverse the current node's pointer to point to the previous node
+            curr.next = prev;
+            // Move the pointers forward
+            prev = curr;
+            curr = tmp;
+        }
+        
+        // At the end, prev will be pointing to the new head of the reversed list
+        return prev;
     }
 }
+
+//  1st way naive approach by using stack 
+//  iterative approach by using three pointer we can change the direction of nodes 
+
