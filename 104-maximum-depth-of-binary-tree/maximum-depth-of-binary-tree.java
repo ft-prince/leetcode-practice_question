70d4f41d52ab13515c6731358b4b1b6e960762c1
Prince <128713671+ft-prince@users.noreply.github.com>
2024-02-09 02:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (79.02%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return dfs(root);   
+    }
+    
+    public int dfs(TreeNode root) {
+        if (root == null) return Integer.MIN_VALUE; // Return 0 if node is null
+        if (root.left == null && root.right == null) return 1; // Return 1 if leaf node
+        
+        int left = dfs(root.left); // Recursively calculate min depth of left subtree
+        int right = dfs(root.right); // Recursively calculate min depth of right subtree
+        
+        // Return the maximum depth of the subtree rooted at the current node
+        return 1 + Math.max(left, right);
+    }
+
+}
