6aa3e073e23f9f9674e7ca1010942953673daba0
Prince <128713671+ft-prince@users.noreply.github.com>
2024-04-21 20:20:03 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (96.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,6 +1,63 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length; // number of rows
+        int m = matrix.length; // number of rows
+        int n = matrix[0].length; // number of columns
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if the first row and column need to be zeroed out
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+        for (int j = 0; j < m; j++) {
+            if (matrix[j][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Mark zeros on first row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // Set zeros based on marks in the first row and column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out the first row if necessary
+        if (firstRowZero) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        // Zero out the first column if necessary
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
+
+/**
+
+  int n = matrix.length; // number of rows
         int m = matrix[0].length; // number of columns
 
         int[] row = new int[n]; // row array
@@ -27,11 +84,12 @@ class Solution {
                 }
             }
         }
-    }
-}
+ */
+
+
 
 /**
-    int n = matrix.length;
+         int n = matrix.length;
         int m = matrix[0].length;
 
         // Mark rows and columns that need to be zeroed out
