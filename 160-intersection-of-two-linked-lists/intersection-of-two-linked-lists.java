6cf525ae394e90586f6ed6eda327ff5b854b4408
Prince <128713671+ft-prince@users.noreply.github.com>
2024-04-22 18:19:33 +0530

    Time: 2 ms (33.99%) | Memory: 48.4 MB (64.61%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,42 +11,45 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-     ListNode a=headA;
-    ListNode b=headB;
-    while(a!=b){
-        if(a!=null){
-            a=a.next;
+    
+       int diff=getDifference( headA, headB);
+           // Initialize pointers to the heads of both lists
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+
+        // Adjust the position of pointers based on the length difference
+        if (diff < 0) {
+            while (diff++ != 0) pointerB = pointerB.next;
+        } else {
+            while (diff-- != 0) pointerA = pointerA.next;
         }
-        else{
-            a=headB;
+        while(pointerA !=null){
+            if (pointerA == pointerB) return pointerA; // Intersection found
+          pointerA=pointerA.next;
+          pointerB=pointerB.next;
         }
-                if(b!=null){
-            b=b.next;
+
+   return null;
+
+    }
+public int getDifference(ListNode head1, ListNode head2) {
+    // Initialize length counters for both lists
+    int len1 = 0, len2 = 0;
+    
+    // Traverse both lists simultaneously to calculate their lengths
+    while (head1 != null || head2 != null) {
+        // If head1 is not null, increment len1 and move head1 to the next node
+        if (head1 != null) {
+            ++len1;
+            head1 = head1.next;
         }
-        else{
-            b=headA;
+        // If head2 is not null, increment len2 and move head2 to the next node
+        if (head2 != null) {
+            ++len2;
+            head2 = head2.next;
         }
     }
-return a;
-    }
-
-// //         ListNode A=headA;
-//         ListNode B=headB;
-//         while(A!=B){
-//             if(A!=null){
-//                 A=A.next;
-//             }
-//             else{
-//                 A=headB;
-//             }
-//             if(B!=null){
-//                 B=B.next;
-//             }
-//             else{
-//                 B=headA;
-//             }
-//         }
-//     return A;
-
-
+    // Return the absolute difference in lengths between the two lists
+    return len1 - len2;
+   }
 }
