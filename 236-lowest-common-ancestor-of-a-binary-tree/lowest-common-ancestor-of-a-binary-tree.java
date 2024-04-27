362ff20a00956084f74f36e7309ac44e97d1e805
Prince <128713671+ft-prince@users.noreply.github.com>
2024-04-27 21:04:44 +0530

    Time: 6 ms (100.00%) | Memory: 44.7 MB (54.77%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,29 +9,16 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Base case: if the root is null, return null
-        if(root == null){
-            return null;
-        }   
-        // If the root is either p or q, return the root
-        if(root == p || root == q){
+        if(root == null) return null;
+        if(root == p || root == q) {
             return root;
         }
-        // Recursively find the lowest common ancestor in the left subtree
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        // Recursively find the lowest common ancestor in the right subtree
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        // If both left and right subtrees return non-null values, then the current root is the lowest common ancestor
-        if(left != null && right != null){
+        TreeNode left=  lowestCommonAncestor(root.left,p,q);   
+        TreeNode right=  lowestCommonAncestor(root.right,p,q);   
+       
+        if (left != null && right != null) {
             return root;
         }
-        // If left is null, it means both nodes are in the right subtree, so return right
-        if(left == null){
-            return right;
-        }
-        // If right is null, it means both nodes are in the left subtree, so return left
-        else{
-            return left;
-        }
+        return (left != null) ? left : right;
     }
 }
