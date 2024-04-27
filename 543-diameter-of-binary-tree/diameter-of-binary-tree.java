16a9979bf64d5d6b99eadef0878395ea31905f59
Prince <128713671+ft-prince@users.noreply.github.com>
2024-04-27 18:22:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (34.90%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,27 +13,21 @@
  *     }
  * }
  */
- 
 class Solution {
-
-    int diameter = 0;
-
+    int diameter=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        dfs(root); // Start DFS traversal from the root node
-        return diameter; // Return the calculated diameter
+        findingDiameter(root);
+        return diameter;
+    }
+    public int findingDiameter(TreeNode node){
+           if(node==null){
+            return 0;
+           }
+
+        int left=findingDiameter(node.left);
+        int right=findingDiameter(node.right);
+        diameter=Math.max(diameter,left+right);
+        return  1 + Math.max(left,right);
     }
 
-    private int dfs(TreeNode current) {
-        if (current == null) {
-            return 0; // Base case: Return 0 if the current node is null (leaf node)
-        }
-        // Recursively compute the depth of the left subtree
-        int left = dfs(current.left);
-        // Recursively compute the depth of the right subtree
-        int right = dfs(current.right);
-        // Update the diameter if the sum of left and right depths is greater
-        diameter = Math.max(diameter, left + right);
-        // Return the depth of the current subtree rooted at the current node
-        return 1 + Math.max(left, right);
-    }
 }
