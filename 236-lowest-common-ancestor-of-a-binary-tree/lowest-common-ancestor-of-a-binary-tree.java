ce165559bee80de47b1a2a17f033bbbfe8aec24b
Prince <128713671+ft-prince@users.noreply.github.com>
2024-04-27 21:05:24 +0530

    Time: 6 ms (100.00%) | Memory: 44.3 MB (86.62%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -1,24 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root == p || root == q) {
+        // Base case: if the root is null, or if either p or q is found, return the root
+
+        if (root == null)
+            return null;
+        if (root == p || root == q) {
             return root;
         }
-        TreeNode left=  lowestCommonAncestor(root.left,p,q);   
-        TreeNode right=  lowestCommonAncestor(root.right,p,q);   
-       
+        // Search for p and q in the left subtree
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        // Search for p and q in the right subtree
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        // If both p and q are found in different subtrees, return the current root as
+        // the LCA
         if (left != null && right != null) {
             return root;
         }
+
+        // If either p or q is found in one of the subtrees, return that node
+        // Otherwise, return the node found in the other subtree (may be null)
         return (left != null) ? left : right;
     }
 }
