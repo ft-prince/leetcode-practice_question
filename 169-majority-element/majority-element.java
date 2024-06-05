aa803de590548608d27ffb8c9eee524c669a4a7a
Prince <128713671+ft-prince@users.noreply.github.com>
2024-06-06 00:32:17 +0530

    Time: 2 ms (76.14%) | Memory: 52.8 MB (59.61%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,49 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-      HashMap<Integer, Integer> map= new HashMap<>();
-        int n = nums.length;
-        for(int i=0; i<n; i++)
-        {
-            if(map.containsKey(nums[i]))
-            map.put(nums[i], map.get(nums[i])+1);
-            else
-            map.put(nums[i],1);
+        int candidate=nums[0];
+        int life=1;
+        for(int i=1;i<nums.length;i++){
+            if(life==0){
+                candidate=nums[i];
+            }
+            if(candidate==nums[i]) life++;
+            else life--;
         }
-        for(int key: map.keySet())
-        {
-            if(map.get(key) > n/2)
-            return key;
-        }
-        return -1;
-
+        return candidate ;
     }
 }
-
-
-//  brute force  using two nested loop 
-//  using HasMap(nums[i],values); then check 
-/*
-     HashMap<Integer, Integer> map= new HashMap<>();
-        int n = nums.length;
-        for(int i=0; i<n; i++)
-        {
-            if(map.containsKey(nums[i]))
-            map.put(nums[i], map.get(nums[i])+1);
-            else
-            map.put(nums[i],1);
-        }
-        for(int key: map.keySet())
-        {
-            if(map.get(key) > n/2)
-            return key;
-        }
-        return -1;
-
-
-2nd 
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-
-
-
- */
