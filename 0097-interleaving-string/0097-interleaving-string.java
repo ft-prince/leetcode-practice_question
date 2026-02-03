6cf7451d67fb0840f91d8028ecbad3264d4d5946
Prince <128713671+ft-prince@users.noreply.github.com>
2026-02-03 23:43:23 +0530

    Time: 4 ms (53.8%), Space: 43.9 MB (14.36%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length())
+            return false;
+        
+         Stack<int[]> stack = new Stack<int[]>();
+        stack.push(new int[] {0, 0});
+		
+        boolean[][] visited = new boolean[s1.length() + 1][s2.length() + 1];
+        
+        while (!stack.empty()) {
+            int[] indexes = stack.pop();
+            int i = indexes[0], j = indexes[1];
+            visited[i][j] = true;
+			
+			// bottom-rightmost coordinate 
+            if (i == s1.length() && j == s2.length())
+                return true;
+            
+			//  if we can increment i (traveling right on the graph)
+            if (i < s1.length() && !visited[i + 1][j] && s1.charAt(i) == s3.charAt(i + j))
+                stack.push(new int[] {i + 1, j});
+            
+			//  if we can increment j (traveling down on the graph)
+            if (j < s2.length() && !visited[i][j + 1] && s2.charAt(j) == s3.charAt(i + j))
+                stack.push(new int[] {i, j + 1});
+        }
+        
+        return false;
+    }
+}
